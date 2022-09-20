1b3ae864eb8ade37181571f816e0b574b7fb80d1
lamkhoi1304 <lamkhoi1304@gmail.com>
2022-09-20 15:52:22 +0700

    Functionize project

--- main.go
+++ main.go
@@ -16,59 +16,47 @@ const (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	if err := checkFolder(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
 
-	if _, err := os.Stat("/tmp/ohmyshell"); os.IsNotExist(err) {
-		err = os.Mkdir("/tmp/ohmyshell", 0755)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
+	if err := checkFile(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
 	}
-	if _, err := os.Stat("/tmp/ohmyshell"); os.IsNotExist(err) {
-		_, err := os.Create("/tmp/ohmyshell/history")
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
+
 	history, err := os.OpenFile("/tmp/ohmyshell/history", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return
 	}
 
 	defer history.Close()
 
-	for {
-		hostname, err := os.Hostname()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		currentUser, err := user.Current()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		path, err := os.Getwd()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		path = strings.Replace(path, "/home/"+currentUser.Username, "~", -1)
+	reader := bufio.NewReader(os.Stdin)
 
-		fmt.Printf(purple, currentUser.Username)
-		fmt.Printf(purple, "@")
-		fmt.Printf(purple, hostname)
-		fmt.Printf(":")
-		fmt.Printf(cyan, path)
-		fmt.Printf("$ ")
+	for {
+		hostname := getHostname()
+		username := getUsername()
+		path := getPath()
+
+		printPrompt(hostname, username, path)
 
 		input, err := reader.ReadString('\n')
+
 		if input == "\n" {
 			continue
 		}
+
 		if _, e := history.Write([]byte(input)); e != nil {
 			fmt.Fprintln(os.Stderr, e)
 		}
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
+
 		if err := execInput(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
@@ -79,55 +67,132 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
 
-	cmd := exec.Command(args[0], args[1:]...)
-
 	switch args[0] {
 	case "cd":
-		if len(args) > 2 {
-			return errors.New("ohmyshell: " + args[0] + ": " + "too many arguments")
-		}
-		if len(args) < 2 {
-			currentUser, err := user.Current()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-			}
-			return os.Chdir(currentUser.HomeDir)
-		}
-		if args[1] == "~" {
-			currentUser, err := user.Current()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-			}
-			return os.Chdir(currentUser.HomeDir)
-		}
-
-		return os.Chdir(args[1])
+		return cdShell(args)
 	case "history":
-		if len(args) > 2 {
-			return errors.New("ohmyshell: " + args[0] + ": " + "too many arguments")
-		}
-		if len(args) < 2 {
-			cmd := exec.Command("cat", "/tmp/ohmyshell/history")
-
-			cmd.Stderr = os.Stderr
-			cmd.Stdout = os.Stdout
-
-			return cmd.Run()
-		}
-		if args[1] == "-c" {
-			cmd := exec.Command("truncate", "-s", "0", "/tmp/ohmyshell/history")
-
-			cmd.Stderr = os.Stderr
-			cmd.Stdout = os.Stdout
-
-			return cmd.Run()
-		}
+		return historyShell(args)
 	case "exit":
-		os.Exit(0)
+		exitShell()
+	default:
+		cmd := exec.Command(args[0], args[1:]...)
+
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+
+		return cmd.Run()
 	}
 
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
+	return nil
+}
 
-	return cmd.Run()
+func exitShell() {
+	os.Exit(0)
+}
+
+func cdShell(args []string) error {
+	if len(args) > 2 {
+		return errors.New("ohmyshell: " + args[0] + ": " + "too many arguments")
+	}
+
+	if len(args) < 2 {
+		currentUser, err := user.Current()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return os.Chdir(currentUser.HomeDir)
+	}
+
+	if args[1] == "~" {
+		currentUser, err := user.Current()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return os.Chdir(currentUser.HomeDir)
+	}
+
+	return os.Chdir(args[1])
+}
+
+func historyShell(args []string) error {
+	if len(args) > 2 {
+		return errors.New("ohmyshell: " + args[0] + ": " + "too many arguments")
+	}
+
+	if len(args) < 2 {
+		cmd := exec.Command("cat", "/tmp/ohmyshell/history")
+
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+
+		return cmd.Run()
+	}
+
+	if args[1] == "-c" {
+		cmd := exec.Command("truncate", "-s", "0", "/tmp/ohmyshell/history")
+
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+
+		return cmd.Run()
+	}
+
+	return nil
+}
+
+func checkFolder() error {
+	if _, err := os.Stat("/tmp/ohmyshell"); !os.IsNotExist(err) {
+		return err
+	}
+
+	return os.Mkdir("/tmp/ohmyshell", 0755)
+}
+
+func checkFile() error {
+	if _, err := os.Stat("/tmp/ohmyshell"); !os.IsNotExist(err) {
+		return err
+	}
+
+	_, err := os.Create("/tmp/ohmyshell/history")
+	return err
+}
+
+func getHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	return hostname
+}
+
+func getUsername() string {
+	username, err := user.Current()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	return username.Username
+}
+
+func getPath() string {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	return homeSign(path, getUsername())
+}
+
+func homeSign(path string, username string) string {
+	return strings.Replace(path, "/home/"+username, "~", -1)
+}
+
+func printPrompt(hostname string, username string, path string) {
+	fmt.Printf(purple, username)
+	fmt.Printf(purple, "@")
+	fmt.Printf(purple, hostname)
+	fmt.Printf(":")
+	fmt.Printf(cyan, path)
+	fmt.Printf("$ ")
 }
